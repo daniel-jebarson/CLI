a3dad3e2b353fc5680b3b01c8a0c4f691aed7e25
Dani-Hacker <88134306+Dani-Hacker@users.noreply.github.com>
2021-07-29 13:36:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+const figlet = require('figlet');
+const chalk = require('chalk');
+const inquirer = require('inquirer');
+const fetch = require('node-fetch');
+
+// const about = async searchQuery => {
+//     const browser = await puppeteer.launch();
+//     const page = await browser.newPage();
+//     await page.goto(`https://www.google.com/search?q=${String(searchQuery).split(" ").join("%20")}`);
+//     await page.goto(`https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${searchQuery}&format=json`)
+//     await page.$eval(``);
+// }
+const about = async searchQuery =>
+    await fetch(`https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${searchQuery}&format=json`)
+        .then(res => res.json())
+        .then(json => console.log('\n', chalk.cyan(String(json.query.search[0].snippet).replace(/<[^>]+>/g, ''))));
+
+figlet.text('Danie-Cyber', 'Slant', (err, res) => {
+    if (err) {
+        console.error('Something is wrong...');
+    }
+    else {
+        console.log(chalk.blueBright(res));
+    }
+
+    // Inquirer stuff
+
+    inquirer.prompt(
+        {
+            type: 'input',
+            name: 'query',
+            message: 'What do you want to search for?',
+            default: 'Jack Sparrow'
+        }
+    ).then(async answer => {
+        console.log(chalk.greenBright(`${answer.query}`));
+        let result = await about(answer.query);
+    })
+});
